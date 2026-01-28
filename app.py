e6685b6d627aa13dc7014704c74d63d2af070f85
Mohd Sharim <mohdsharim982673@gmail.com>
2026-01-28 17:45:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://localhost:8000"
+API_URL = "https://costumer-churn-prediction-1.onrender.com"
 
 st.set_page_config(page_title="Churn Predictor", page_icon="📊")
 
